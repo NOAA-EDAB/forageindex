c999bc5ec58ff93dfa9b65e966f588a65717dbdb
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-10-27 12:12:21 -0400

    updated inputs to 2022

--- fhdat/VASTforage_ProcessInputDat.R
+++ fhdat/VASTforage_ProcessInputDat.R
@@ -292,14 +292,104 @@ bluepyagg_stn_all <- left_join(bluepyagg_stn_all, NEAMAPidSST, by="id") %>%
 
 #make an SST dataframe for 2022! Add to saved sst_data in data-raw/gridded
 
-
-
-#read in diet data with month day fields
-
 library(sf)
 library(raster)
 library(terra)
 library(nngeo)
+
+# Bastille function from https://github.com/kimberly-bastille/ecopull/blob/main/R/utils.R
+
+nc_to_raster <- function(nc,
+                         varname,
+                         extent = c(0, 360, -90, 90),
+                         crop = raster::extent(280, 300, 30, 50),
+                         show_images = FALSE) {
+  
+  message("Reading .nc as brick...")
+  
+  r <- raster::brick(nc, varname = varname)
+  
+  message("Setting CRS...")
+  raster::crs(r) <- "+proj=longlat +lat_1=35 +lat_2=45 +lat_0=40 +lon_0=-77 +x_0=0 +y_0=0 +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+  
+  # not sure if this is necessary?
+  raster::extent(r) <- raster::extent(extent)
+  
+  if(show_images){
+    par(mfrow = c(1,2))
+    raster::plot(r, 1, sub = "Full dataset")
+  }
+  
+  message("Cropping data...")
+  ne_data <- raster::crop(r, crop)
+  #ne_data <- raster::rotate(ne_data) add here for future pulls
+  
+  if(show_images){
+    raster::plot(ne_data, 1, sub = "Cropped dataset")
+    par(mfrow = c(1,1))
+  }
+  
+  message("Done!")
+  
+  return(ne_data)
+}
+
+# function to convert to dataframe based on
+# https://towardsdatascience.com/transforming-spatial-data-to-tabular-data-in-r-4dab139f311f
+
+raster_to_sstdf <- function(brick,
+                            rotate=TRUE){
+  
+  if(rotate) brick_r <- raster::rotate(brick)
+  brick_r <- raster::crop(brick_r, raster::extent(-77,-65,35,45))
+  sstdf <- as.data.frame(raster::rasterToPoints(brick_r, spatial = TRUE))
+  sstdf <- sstdf %>%
+    dplyr::rename(Lon = x,
+                  Lat = y) %>%
+    tidyr::pivot_longer(cols = starts_with("X"),
+                        names_to = c("year", "month", "day"),
+                        names_prefix = "X",
+                        names_sep = "\\.",
+                        values_to = "sst",
+    )
+  return(sstdf)
+}
+
+# pull the OISST data as raster brick, modified from 
+# https://github.com/kimberly-bastille/ecopull/blob/main/.github/workflows/pull_satellite_data.yml
+
+varname <- "sst"
+
+years <- 2022
+for(i in years) {
+  name <- paste0(i, ".nc")
+  dir.create(here::here("data-raw","gridded", "sst_data"), recursive = TRUE)
+  filename <- here::here("data-raw","gridded", "sst_data", paste0("test_", i, ".grd"))
+  url <- paste0("https://downloads.psl.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.", i, ".nc")
+  download.file(url, destfile = name)
+  
+  text <- knitr::knit_expand(text = "test_{{year}} <- nc_to_raster(nc = name, varname = varname)
+                                     raster::writeRaster(test_{{year}}, filename = filename, overwrite=TRUE)",
+                             year = i)
+  print(text)
+  try(eval(parse(text = text)))
+  unlink(name) # remove nc file to save space
+  print(paste("finished",i))
+}
+
+
+# convert raster to dataframe
+#years <- 2022
+for(i in years) {
+  name <- get(paste0("test_",i))
+  filename <- here::here("data-raw","gridded", "sst_data", paste0("sst", i, ".rds"))
+  text <- knitr::knit_expand(text = "sst{{year}} <- raster_to_sstdf(brick = name)
+                                     saveRDS(sst{{year}}, filename)",
+                             year = i)
+  print(text)
+  try(eval(parse(text = text)))
+}
+#read in diet data with month day fields
 
 #bluepyagg_stn_all <- readRDS(here("fhdat/bluepyagg_stn_all.rds"))
 
@@ -380,4 +470,4 @@ dietstn_OISST_merge <- dietstn_OISST %>%
 
 bluepyagg_stn_all_OISST <- left_join(bluepyagg_stn_all, dietstn_OISST_merge)
 
-#saveRDS(bluepyagg_stn_all_OISST, here("fhdat/bluepyagg_stn_all_OISST.rds"))
+saveRDS(bluepyagg_stn_all_OISST, here("fhdat/bluepyagg_stn_all_OISST_1973-2022.rds"))
