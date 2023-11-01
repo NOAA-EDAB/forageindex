4426dd5c290c8fcdb613b487e5cdff1bd7003fe0
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-10-31 21:40:25 -0400

    change units and target file, no annual

--- SOEpyindex/SOE-VASTForageIndices.R
+++ SOEpyindex/SOE-VASTForageIndices.R
@@ -51,7 +51,7 @@ SOEinputs <- function(infile, season, outfile) {
     tidyr::pivot_longer(c("Forage Fish Biomass Estimate", "Forage Fish Biomass Estimate SE"), 
                         names_to = "Var", values_to = "Value") %>%
     dplyr::filter(EPU %in% c("MAB", "GB", "GOM", "AllEPU")) %>%
-    dplyr::mutate(Units = "grams per stomach") %>%
+    dplyr::mutate(Units = "relative grams per stomach") %>%
     dplyr::select(Time, Var, Value, EPU, Units)
   
   forageindex$Var <- stringr::str_c(season, forageindex$Var, sep = " ")
@@ -63,17 +63,17 @@ SOEinputs <- function(infile, season, outfile) {
 
 
 # make data files
-SOEinputs(infile = "pyindex/allagg_fall_500_lennosst_ALLsplit_biascorrect/Index.csv",
+SOEinputs(infile = "SOEpyindex/1982-2022/allagg_fall_500_lennosst_ALLsplit_biascorrect/Index.csv",
           season = "Fall", 
-           outfile = "toSOE/fallforageindex.rds")
+           outfile = "SOEpyindex/fallforageindex.rds")
 
-SOEinputs(infile = "pyindex/allagg_spring_500_lennosst_ALLsplit_biascorrect/Index.csv",
+SOEinputs(infile = "SOEpyindex/1982-2022/allagg_spring_500_lennosst_ALLsplit_biascorrect/Index.csv",
           season = "Spring", 
-           outfile = "toSOE/springforageindex.rds")
+           outfile = "SOEpyindex/springforageindex.rds")
 
-SOEinputs(infile = "pyindex/allagg_annual_500_lennosst_ALLsplit_biascorrect/Index.csv",
-          season = "Annual", 
-           outfile = "toSOE/annualforageindex.rds")
+# SOEinputs(infile = "pyindex/allagg_annual_500_lennosst_ALLsplit_biascorrect/Index.csv",
+#           season = "Annual", 
+#            outfile = "toSOE/annualforageindex.rds")
 
 
 
